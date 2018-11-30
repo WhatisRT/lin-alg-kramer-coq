288adb224278f522443a001f500de4325c7af945
Andre Knispel <andre@Andres-MacBook-Pro-2.local>
2018-11-30 22:53:35 +0100

    Add definitions for injective, surjective and bijective.

--- Blatt1.v
+++ Blatt1.v
@@ -1,57 +1,48 @@
 Require Import CpdtTactics.
-Require Import QArith.Qring.
-Require Import QArith.Qfield.
-Require Import Lists.List.
-Require Import List.
-Require Import Fin.
-Import ListNotations.
+Require Import Classes.RelationClasses.
+Require Import Classes.Morphisms.
 
-Fixpoint finList (k : nat) : list (Fin.t k) :=
-  match k with
-  | O => List.nil
-  | S m => List.cons Fin.F1 (List.map (fun x => Fin.FS x) (finList m))
-  end.
+Set Implicit Arguments.
 
-Require Import QArith.QArith_base.
+Section FunctionDefinitions.
+  Variable A B : Type.
+  Variable eq_A : A -> A -> Prop.
+  Variable eq_B : B -> B -> Prop.
 
-Definition sum x := List.fold_left Qplus x 0.
+  Variable eqiv_A : Equivalence eq_A.
+  Variable eqiv_B : Equivalence eq_B.
 
-Definition iN n := inject_Z (Z.of_nat n).
+  Variable f : A -> B.
 
-Definition finListNatQ (k : nat) : list Q := List.map (fun k => iN (proj1_sig (to_nat k))) (finList k).
+  Variable f_comp : Proper (eq_A==>eq_B) f.
 
-Lemma gauss : forall n, let nQ := iN n in
-                   sum (finListNatQ (S n)) == Qdiv (nQ * (nQ + 1)) (iN 2).
-Proof.
-  Hint Unfold finListNatQ.
-  Hint Unfold sum.
-  Hint Unfold iN.
-  Hint Unfold inject_Z.
-  Hint Unfold Z.of_nat.
-  (* Hint Unfold Qmult. *)
-  (* Hint Unfold Qdiv. *)
-  (* Hint Unfold Qinv. *)
+  Notation "x ==_A y" := (eq_A x y)(at level 70, no associativity).
+  Notation "x ==_B y" := (eq_B x y)(at level 70, no associativity).
   
-  induction n; crush.
-  assert (finListNatQ (S (S n)) = finListNatQ (S n) ++ [iN (S n)]).
-  {
-    clear IHn.
-    generalize dependent n.
-    induction n.
-    - crush.
-    - admit. 
-  }
-  rewrite H.
-  unfold sum in *.
-  rewrite fold_left_app.
-  simpl.
-  setoid_rewrite IHn.
-  setoid_replace (iN (S n)) with (iN n + 1). field_simplify.
-  remember (iN n) as k.
-  unfold iN. unfold inject_Z, Z.of_nat. field_simplify. setoid_reflexivity.
-  1,2:autounfold with *; crush.
-  unfold iN.
-  rewrite Nat2Z.inj_succ. 
-  setoid_replace 1 with (inject_Z 1) by setoid_reflexivity.
-  setoid_rewrite <- inject_Z_plus. setoid_reflexivity.
-Admitted.
+  Definition injective := forall x1 x2 : A, f x1 ==_B f x2 -> x1 ==_A x2.
+  Definition surjective := forall y : B, exists x, f x ==_B y.
+  Definition bijective := injective /\ surjective.
+End FunctionDefinitions.
+
+Require Import Arith.PeanoNat.
+Import Nat.
+
+Section Ex1a.
+  Definition twonat := { x : nat | Even x }.
+  Program Definition f : nat -> twonat :=
+    fun x => 2 * x.
+  Next Obligation.
+    match goal with
+    | [ H: nat |- _ ] => exists H; crush
+    end.
+  Defined.
+
+  Check Logic.eq.
+  Search (forall A, A -> A -> Prop).
+
+  Lemma f_bij : bijective eq Logic.eq f.
+    split.
+    - unfold injective.
+    crush.
+
+
